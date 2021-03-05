66b5c13a22a14d8436324f36ce7843511f659003
abdularahman <abdularahman@d7oom-pc>
2021-03-05 23:53:42 +0300

    LastElementOnArray

---
+++ LastElementOnArray/src/lastelementonarray/LastElementOnArray.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lastelementonarray;
+
+/**
+ *
+ * @author abdularahman
+ */
+public class LastElementOnArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+    }
+   public static int lastElm(int[] arr) {
+  // write your code here
+  
+  
+  return arr[arr.length-1];
+} 
+}
