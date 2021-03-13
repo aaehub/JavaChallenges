5a2f5be4959ba22f4dc7d6509ca7f8611125287d
abdularahman <abdularahman@d7oom-pc>
2021-03-13 07:51:13 +0300

    CircumferenceOfRhombus

---
+++ circumferenceOfRhombus/src/circumferenceofrhombus/CircumferenceOfRhombus.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package circumferenceofrhombus;
+
+/**
+ *
+ * @author abdularahman
+ */
+public class CircumferenceOfRhombus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static double circumferenceOfRhombus(double length) {
+	// write your code here
+        
+        return length*4;
+}
+}
