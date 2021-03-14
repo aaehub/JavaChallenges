35334fddf24a73e1d7b416ff200bbb8b5554b2e5
abdularahman <abdularahman@d7oom-pc>
2021-03-14 06:48:08 +0300

    CubeRoot

---
+++ cubeRoot/src/cuberoot/CubeRoot.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuberoot;
+
+/**
+ *
+ * @author Abdualrahman-AAE
+ */
+public class CubeRoot {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+ 
+        System.out.println(squares(8.0)) ;
+    
+    }
+    public static double squares(double num) {
+	// write your code here
+       
+       
+        
+        
+        
+        return  Math.cbrt(num);
+  
+  
+}
+}
