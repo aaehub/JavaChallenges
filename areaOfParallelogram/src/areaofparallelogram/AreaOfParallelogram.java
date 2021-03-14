7bc781e4262998eb5a117023e4de7b39455a4b8d
abdularahman <abdularahman@d7oom-pc>
2021-03-14 10:13:41 +0300

    AreaOfParallelogram

---
+++ areaOfParallelogram/src/areaofparallelogram/AreaOfParallelogram.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package areaofparallelogram;
+
+/**
+ *
+ * @author Abdualrahman-AAE
+ */
+public class AreaOfParallelogram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static double areaOfParallelogram(double height, double base) {
+	// write your code here
+        
+        
+        
+        return height*base;
+        
+}
+}
