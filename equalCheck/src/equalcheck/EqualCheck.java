c412e07b8759d0dd7b2f1222183fabcd7dbb0343
abdularahman <abdularahman@d7oom-pc>
2021-03-14 03:58:44 +0300

    EqualCheck

---
+++ equalCheck/src/equalcheck/EqualCheck.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package equalcheck;
+
+/**
+ *
+ * @author Abdualrahman-AAE
+ */
+public class EqualCheck {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static boolean equalCheck(int num1, int num2) {
+	// write your code here
+
+       boolean isIt=false;
+  
+       if(num1==num2){
+           isIt=true;
+       }
+       return isIt;
+}
+}
