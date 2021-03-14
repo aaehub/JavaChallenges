74ca2df1a99579d872ee0e9890e2ba9d80996494
abdularahman <abdularahman@d7oom-pc>
2021-03-14 04:00:59 +0300

    Exponent_squared

---
+++ exponent_squared/src/exponent_squared/Exponent_squared.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exponent_squared;
+
+/**
+ *
+ * @author Abdualrahman-AAE
+ */
+public class Exponent_squared {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static int exponent_squared(int number) {
+  // write your code here
+        return number*number;
+}
+}
