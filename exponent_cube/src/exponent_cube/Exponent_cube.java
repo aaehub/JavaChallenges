db8b29b7df4f9ca973a712708f0eda98cd9d6a35
abdularahman <abdularahman@d7oom-pc>
2021-03-14 04:02:18 +0300

    Exponent_cube

---
+++ exponent_cube/src/exponent_cube/Exponent_cube.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exponent_cube;
+
+/**
+ *
+ * @author Abdualrahman-AAE
+ */
+public class Exponent_cube {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static int exponent_cube(int number) {
+  // write your code here
+  return number*number*number;
+}
+}
