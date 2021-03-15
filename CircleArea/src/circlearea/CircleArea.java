1560425d1b694f6b0b5a77bdfb70be94fd1091c2
abdularahman <abdularahman@d7oom-pc>
2021-03-15 16:01:54 +0300

    CircleArea

---
+++ CircleArea/src/circlearea/CircleArea.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package circlearea;
+
+/**
+ *
+ * @author Abdualrahman-AAE
+ */
+public class CircleArea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+
+public static double findCircleArea(double radius) {
+	// write your code here
+        return radius*radius*3.14;
+}
+}
