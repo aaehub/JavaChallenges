776a565a3182cbe06af9af7cd1b17a5f07c49515
abdularahman <abdularahman@d7oom-pc>
2021-03-15 16:10:27 +0300

    SequentialAddition

---
+++ Sequential addition/src/sequential/addition/SequentialAddition.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sequential.addition;
+
+/**
+ *
+ * @author Abdualrahman-AAE
+ */
+public class SequentialAddition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(numberSum(5));
+    }
+   public static int numberSum(int num) {
+	// write your code here
+        int sum=0;
+        for(int i=num;i<=num&&i>0;i--){
+            sum+=i;
+        
+        
+        
+        
+        }
+       return sum;
+   
+   } 
+}
