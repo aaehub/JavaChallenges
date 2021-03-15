d7c1ff40d115d1cf673dd1923942356d4600c99a
abdularahman <abdularahman@d7oom-pc>
2021-03-15 16:04:09 +0300

    AreaOfRhombus

---
+++ areaOfRhombus/src/areaofrhombus/AreaOfRhombus.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package areaofrhombus;
+
+/**
+ *
+ * @author Abdualrahman-AAE
+ */
+public class AreaOfRhombus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static double areaOfRhombus(double height, double length) {
+	// write your code here
+	
+	
+	
+	return height * length/2 ;
+
+	
+}
+}
