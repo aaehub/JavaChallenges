105517ebb2f6fe8b8f411189c50fdddec593a586
abdularahman <abdularahman@d7oom-pc>
2021-03-18 16:34:31 +0300

    convertHoursToSeconds

---
+++ hour/src/hour/Hour.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hour;
+
+/**
+ *
+ * @author Abdualrahman-AAE
+ */
+public class Hour {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println(convertToSeconds(1));
+        
+    }
+    public static int convertToSeconds(int hours) {
+	// write your code here
+        
+        
+        return hours*3600;
+}
+    
+    
+    
+}
