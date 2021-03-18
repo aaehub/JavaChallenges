387ebec0ac92103029722f32037077f10f40e582
abdularahman <abdularahman@d7oom-pc>
2021-03-18 11:23:51 +0300

    AreaOfRectangle

---
+++ areaOfRectangle/src/areaofrectangle/AreaOfRectangle.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package areaofrectangle;
+
+/**
+ *
+ * @author Abdualrahman-AAE
+ */
+public class AreaOfRectangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static int areaOfRectangle(int width, int height) {
+  // write your code here
+  
+  
+    return width *height;
+}
+}
